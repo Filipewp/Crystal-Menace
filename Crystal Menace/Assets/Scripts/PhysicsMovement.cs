c75fa482fd9840fd3bd89ec2ae8ebaad4b46a3db
Filipewp <filipepatricio@hotmail.com.br>
2020-09-11 15:58:48 -0300

    ModelsAndLevels

---
+++ Crystal Menace/Assets/Scripts/PhysicsMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsMovement : MonoBehaviour
+{
+    public float speed = 100f;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(xMov, rb.velocity.y, zMov) * speed * Time.deltaTime;
+    }
+}
